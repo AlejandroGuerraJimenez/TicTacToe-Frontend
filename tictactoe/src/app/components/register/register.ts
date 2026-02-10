d52953b02be220b3bedbfa5abd33d15423d876d6
AlejandroGuerraJimenez <alejandro.guerra111@alu.ulpgc.es>
2026-02-10 16:26:34 +0000

    Fixed communication errors and added login component

---
+++ tictactoe/src/app/components/register/register.ts
@@ -0,0 +1,34 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule],
+  templateUrl: './register.html',
+})
+export class RegisterComponent {
+  
+  private authService = inject(AuthService);
+
+  username: string = '';
+  email: string = '';
+  password: string = '';
+
+  onRegister() {
+    const userData = {
+      username: this.username,
+      email: this.email,
+      password: this.password
+    };
+
+    this.authService.register(userData).subscribe({
+      next: (response) => {
+        console.log('Registro exitoso:', response);
+      },
+      error: (error) => {
+        console.error('Error al registrar:', error);
+      }
+    });
+  }
+}
